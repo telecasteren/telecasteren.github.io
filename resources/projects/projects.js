44d97730cdbdbe9028b3bd122d18e2045158575f
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-02-18 17:43:16 +0100

    shortened video recordings for performance and load

--- resources/projects/projects.js
+++ resources/projects/projects.js
@@ -5,9 +5,9 @@ const projects = [
     img_src: "/resources/images/unwired-blob-dark.png",
     img_alt: "A tech-blog website",
     proj_url: "https://unwired.netlify.app/",
-    git_repo: "https://github.com/telecasteren/project-exam-1-tele-c.git",
     proj_video:
-      "/resources/videos/recording-unwired 2025-02-12 at 22.11.23.mov",
+      "/resources/videos/Unwired-Recording 2025-02-18 at 17.36.57.mov",
+    git_repo: "https://github.com/telecasteren/project-exam-1-tele-c.git",
     description:
       "Unwired is a tech blog that blends personal experiences with industry insights—served with a satirical twist.",
   },
@@ -19,7 +19,7 @@ const projects = [
     img_alt: "An ecommerce video-game website",
     proj_url: "https://gamehub-tele.netlify.app/",
     proj_video:
-      "/resources/videos/Recording-gamehub 2025-02-13 at 06.46.20.mov",
+      "/resources/videos/Gamehub-Recording 2025-02-18 at 17.34.43.mov",
     git_repo: "https://github.com/telecasteren/gamehub.git",
     description:
       "An e-commerce website for the gaming community, bringing sellers and buyers together.",
@@ -32,7 +32,7 @@ const projects = [
     img_alt: "A website for a community museum",
     proj_url: "https://telecasteren-semester1.netlify.app/",
     proj_video:
-      "/resources/videos/Recording-semester1-proj 2025-02-13 at 06.49.11.mov",
+      "/resources/videos/SemProj1-Recording 2025-02-18 at 17.36.06.mov",
     git_repo: "https://github.com/telecasteren/semesterProject1.git",
     description:
       "A website for a family museum. Built with pure vanilla HTML and CSS.",
