572263fe6f4fb5e907096bdb6f02f970dd360b55
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-02-19 10:11:26 +0100

    file path correction

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import { colorModeToggle } from "/js/app/components/colorModes/switch.js";
 import { switchColorMode } from "/js/app/components/colorModes/toggleEvents.js";
 import { menu } from "/js/app/components/navigation/menu.js";
 import { welcomeMessage } from "/js/app/landing/welcomeMessage.js";
-import { renderContent } from "/js/app/eventListeners/renderContent.js";
+import { renderContent } from "/js/app/UI/renderContent.js";
 
 // COLORMODE
 colorModeToggle();
