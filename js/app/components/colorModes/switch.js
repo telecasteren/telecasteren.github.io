4c1920af23133b4cd0e406ff780e05e7e02cce12
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-06-19 13:46:26 +0200

    fixed a bug that resulted in coloormode btn text disappearing upon hashchange

--- js/app/components/colorModes/switch.js
+++ js/app/components/colorModes/switch.js
@@ -2,12 +2,15 @@ import { switchColorMode } from "/js/app/components/colorModes/toggleEvents.js";
 import { pageContent } from "/js/app/utils/constants.js";
 
 export function colorModeToggle() {
+  const storedColorMode = window.localStorage.getItem("colorMode");
+
   const toggleContainer = document.createElement("div");
   toggleContainer.className = "toggleContainer";
 
   const toggleSwitch = document.createElement("div");
   toggleSwitch.classList.add("toggleSwitch", "glowBorderBtn");
-  // toggleSwitch.innerText = "DarkMode/LightMode";
+  toggleSwitch.innerText =
+    storedColorMode === "dark" ? "Turn the light on" : "Turn the light off";
 
   pageContent.prepend(toggleSwitch);
 
